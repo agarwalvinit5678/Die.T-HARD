e68ecdf3aa75cbeb8cc6b19f4d1af6c5a7842009
agarwalvinit5678 <92209640+agarwalvinit5678@users.noreply.github.com>
2022-01-08 14:52:14 +0530

    Update home.js

--- home.js
+++ home.js
@@ -19,3 +19,17 @@ $( "#signupbtn" ).remove();
 $('#logout').click(function (){
     login_value=false;
 });
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
