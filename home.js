19f16676b2a017bb91f5c428d8611abed740f915
agarwalvinit5678 <92209640+agarwalvinit5678@users.noreply.github.com>
2022-01-08 10:16:23 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,21 @@
+
+
+var login_value=false;
+if(login_value===false)
+{
+  $("#tracker").attr("data-toggle","modal");
+  $("#goal").attr("data-toggle","modal");
+  $("#profile").attr("data-toggle","modal");
+
+}
+if(login_value===true)
+{
+$("#tracker").attr("data-toggle","");
+$("#goal").attr("data-toggle","");
+$("#profile").attr("data-toggle","");
+$( "#signinbtn" ).remove();
+$( "#signupbtn" ).remove();
+}
+$('#logout').click(function (){
+    login_value=false;
+});
