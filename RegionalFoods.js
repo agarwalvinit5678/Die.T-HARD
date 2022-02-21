54e58acef5e22a8a2d1c6db3ed0018e6b7d65745
Kanishka <kanishka.bansode@gmail.com>
2022-02-22 02:16:09 +0530

    initial region wise data

---
+++ RegionalFoods.js
@@ -0,0 +1,172 @@
+
+var regionalFoodItems= {
+
+    awadhi :{
+
+
+        seekhKebab: "seekh kebab" ,
+        shamiKebab: "shami kebab" ,
+        kakhoriKebab : "kakhori kebab",
+        shahiKorma : "shahi korma" ,
+        naan : "naan" ,
+        paratha : "paratha",
+        chapati : "chappati",
+        rumaliRoti : "rumali roti" ,
+        butter:"butter" ,
+        ghee:"ghee" ,
+
+
+    }   ,
+    bhojpuri:{
+
+        masoorDal:"masoor dal",
+        uradDal : "urad dal",
+        pulao : "pulao",
+        sabjiKorma : "sabji korma",
+        cholePuri: "chole puri",
+        naan : "naan" ,
+        paratha : "paratha",
+        chapati : "chappati",
+        rice : "rice",
+        rajma : "rajma",
+        butter:"butter" ,
+        ghee:"ghee" ,
+        
+
+    }   ,
+    kashmiri :{
+
+        lahradarKebab :"lahradar kabab",
+        nadarMaund :"nadur maund",
+        methiMaaz :"methi maaz",
+        goshtabeh : "goshtabeh",
+        naan : "naan" ,
+        paratha : "paratha",
+        chapati : "chappati",
+        rice : "rice",
+        rajma : "rajma",
+        butter:"butter" ,
+        ghee:"ghee" ,
+
+    }   ,
+    rajisthani :{
+        dalBhati : "dal bhati" ,
+        bajraKiRoti : "bajra ki roti" ,
+        pittodKiSabji : "pittod ki sabji" ,
+        laukiRaKofte : "roki ra kofte" ,
+        matarRoSaag: "matar ro saag" , 
+        chapati : "chappati",
+        rice : "rice",
+        rajma : "rajma",
+        butter:"butter" ,
+        ghee:"ghee" ,
+
+
+    }   ,
+    hyderabadi :{
+        biryani:"hyderabadi biryani" ,
+        hyderabadiHaleem : "hyderabadi haleem" ,
+        patharKaGhosht :"Pathar-ka-Gosht" ,
+        talawaGhost :"talawa ghost" ,
+        shirmal :"shirmal",
+        chapati : "chappati",
+        rice : "rice",
+        butter:"butter" ,
+        ghee:"ghee" ,
+
+    }   ,
+    udupi   :{
+        dosa :"dosa" ,
+        uttappa : "uttappa",
+        sambhar : "sambhar",
+        meduwada : "meduwada",
+        rassam : "rassam" , 
+        masalaDosa : "masala dosa" ,
+        chapati : "chappati",
+        rice : "rice",
+        butter:"butter" ,
+        ghee:"ghee" ,
+    }   ,
+    bengali : {
+        rohu :"rohu",
+        bangda : "bangda",
+        papda : "papda",
+        pompret : "pompret",
+        rasgulla : "rasgulla",
+        chapati : "chappati",
+        rice : "rice",
+        butter:"butter" ,
+        ghee:"ghee" ,
+    }   ,
+
+    goan :{
+        pompret : "pompret",
+        prawns : "prawns",
+        tigerPrawns: "tiger prawns",
+        lobster : "lobster",
+        tuna : tuna ,
+        bebinca : "bebinca",
+        goanSoup : "goan soup",
+        kismur : "kismur" ,
+        dangar : "dangar" ,
+        fishSuke : "fish suke" ,
+        chapati : "chappati",
+        rice : "rice",
+        butter:"butter" ,
+        ghee:"ghee" ,
+    }   ,
+    maharashtrian :{
+        misal:"misal" ,
+        pohe : "pohe",
+        TamdaRassa:"tamda rassa",
+        malvaniChicken : "malvani chicken",
+        bangadaFry : "bangda fry",
+        chapati : "chappati",
+        rice : "rice",
+        butter:"butter" ,
+        ghee:"ghee" ,
+    }   ,
+    assamesse :{
+
+        boraSaul:"bora saul",
+        masTenga : "mas tenga" ,
+        hukoti : "hukoti" ,
+        pitika : "pitika" ,
+        masorTenga : "masor tenga" ,
+        chapati : "chappati",
+        rice : "rice",
+        butter:"butter" ,
+        ghee:"ghee" ,
+
+
+    }   ,
+    manipuri:{
+        nganam: "nganam"    ,
+        paknam : "paaknam" ,
+        singju : "singju" ,
+        chamthong : "chamthong" ,
+        sanaThongba : "sana thongba",
+        rice : "rice",
+        butter:"butter" ,
+        ghee:"ghee" ,
+
+    }
+
+
+
+
+
+        
+
+    
+    
+
+
+
+
+
+}
+
+
+//require() this data whenever necessary 
+exports.regionalFoodItems = regionalFoodItems ;
