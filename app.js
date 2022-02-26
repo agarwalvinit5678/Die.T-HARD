f7e71aff474c30049bafbd3e63816829f7cef846
agarwalvinit5678 <92209640+agarwalvinit5678@users.noreply.github.com>
2022-02-26 21:05:38 +0530

    tracker working

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var login_value=false;
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate=require('mongoose-findorcreate');
 const app = express();
+const regionalFoodItems=require("./RegionalFoods.js");
 app.use(session({
   secret:"this is secret",
   resave:false,
@@ -36,9 +37,13 @@ const userSchema=new mongoose.Schema({
   gender: String,
   allergens: Array,
   bmr:Number,
+  pdct:Number,
   activitylevel: String,
   goal:String,
   split:String,
+  protein:Number,
+  carbohydrate:Number,
+  fat:Number,
 
 });
 
@@ -100,7 +105,7 @@ app.get("/lessons",function(req,res){
 app.get("/goal",function(req,res){
   if(req.isAuthenticated()){
     
-    res.render("goal",{login_value:req.isAuthenticated()});
+    res.render("goal",{login_value:req.isAuthenticated(),protein:req.user.protein,carbohydrate:req.user.carbohydrate,fat:req.user.fat});
   }
   else{
     res.redirect("/");
@@ -147,7 +152,38 @@ app.post("/profile",function(req,res){
   {
      Bmr=447.593+(9.247*req.user.weight)+(3.098*req.user.height)-(4.330*req.user.age);
   }
-    User.findOneAndUpdate({_id:req.user._id}, {split:req.body.split,goal:req.body.goal,name:req.body.name,activitylevel:req.body.activitylevel,weight:req.body.weight,height:req.body.height,age:req.body.age,email:req.body.username,cuisine:req.body.cuisine,gender:req.body.gender,allergens:allerging,username:req.body.username, bmr:Bmr},{new:true}, function(err, doc) {
+
+  if(req.body.activitylevel=="sedentary")
+  var activitymultiplier=1.200;
+  else if(req.body.activitylevel=="lightlyactive")
+  var activitymultiplier=1.375;
+  else if(req.body.activitylevel=="moderatelyactive")
+  var activitymultiplier=1.550;
+  else if(req.body.activitylevel=="veryactive")
+  var activitymultiplier=1.725;
+  else if(req.body.activitylevel=="extraactive")
+  var activitymultiplier=1.900;
+
+  if(req.body.goal=="retain")
+  var goalscenario=1.000;
+  else if(req.body.goal=="gain")
+  var goalscenario=1.100;
+  else if(req.body.goal=="lose")
+  var goalscenario=0.900;
+  var Pdct=parseInt((Bmr*activitymultiplier)*goalscenario);
+  if(req.body.split=="standard")
+  {var Protein = Pdct * 0.25;
+  var Carbohydrate = Pdct * 0.45;
+  var Fat = Pdct * 0.30;}
+  else if(req.body.split=="protein")
+  {var Protein = Pdct * 0.40;
+  var Carbohydrate = Pdct * 0.35;
+  var Fat = Pdct * 0.25;}
+  else if(req.body.split=="carbohydrate")
+  {var Protein = Pdct * 0.35;
+  var Carbohydrate = Pdct * 0.10;
+  var Fat = Pdct * 0.55;}
+  User.findOneAndUpdate({_id:req.user._id}, {split:req.body.split,goal:req.body.goal,name:req.body.name,activitylevel:req.body.activitylevel,weight:req.body.weight,height:req.body.height,age:req.body.age,email:req.body.username,cuisine:req.body.cuisine,gender:req.body.gender,allergens:allerging,username:req.body.username, bmr:Bmr,pdct:Pdct,protein:Protein,carbohydrate:Carbohydrate,fat:Fat},{new:true}, function(err, doc) {
     if(err) return console.log(err);});
   // req.user.name=req.body.name;
   
@@ -159,8 +195,98 @@ res.redirect("/profile");
 });
 app.get("/tracker",function(req,res){
   if(req.isAuthenticated()){
-    
-    res.render("tracker",{login_value:req.isAuthenticated()});
+    var Bprotein=parseInt(req.user.protein*0.25);
+    var Bcarbohydrate=parseInt(req.user.carbohydrate*0.25);
+    var Bfat=parseInt(req.user.fat*0.25);
+    var Lprotein=parseInt(req.user.protein*0.35);
+    var Lcarbohydrate=parseInt(req.user.carbohydrate*0.35);
+    var Lfat=parseInt(req.user.fat*0.35);
+    var Dprotein=parseInt(req.user.protein*0.40);
+    var Dcarbohydrate=parseInt(req.user.carbohydrate*0.40);
+    var Dfat=parseInt(req.user.fat*0.40);
+    var Bcal=parseInt(req.user.pdct*0.25);
+    var Lcal=parseInt(req.user.pdct*0.35);
+    var Dcal=parseInt(req.user.pdct*0.40);
+    var cuisinename=req.user.cuisine;
+    //console.log(regionalFoodItems[cuisinename]);
+    var foodobj=regionalFoodItems[cuisinename];
+    var nonbreadobj=foodobj["nonBread"];
+    var breadobj=foodobj["breads"];
+    var nonbreadlength = Object.keys(nonbreadobj).length;
+    var bbreadlength = Object.keys(breadobj).length;
+   // console.log(nonbreadlength);
+    var bnon= Math.floor(Math.random() * nonbreadlength);    
+    var bbread= Math.floor(Math.random() * bbreadlength);
+    var lnon= Math.floor(Math.random() * nonbreadlength);
+    var lbread= Math.floor(Math.random() * bbreadlength);
+    var dnon= Math.floor(Math.random() * nonbreadlength);
+    var dbread= Math.floor(Math.random() * bbreadlength);
+    var bbreaddish=Object.keys(breadobj)[bbread];
+    console.log(breadobj[bbreaddish].calories);
+    var bnondish=Object.keys(nonbreadobj)[bnon];
+    var lbreaddish=Object.keys(breadobj)[lbread];
+    var lnondish=Object.keys(nonbreadobj)[lnon];
+    var dbreaddish=Object.keys(breadobj)[dbread];
+    var dnondish=Object.keys(nonbreadobj)[dnon];
+    var bcalfullfilltemp=breadobj[bbreaddish].calories+nonbreadobj[bnondish].calories;
+    var bservingsizetemp=(Bcal/bcalfullfilltemp);
+    var bservingsize=parseInt(bservingsizetemp*(breadobj[bbreaddish].servingSize+nonbreadobj[bnondish].servingSize));
+    var bcalfullfill=parseInt(bservingsizetemp*(breadobj[bbreaddish].calories+nonbreadobj[bnondish].calories));
+    var bcarbfullfill=parseInt(bservingsizetemp*(breadobj[bbreaddish].carbs+nonbreadobj[bnondish].carbs));
+    var bfatfullfill=parseInt(bservingsizetemp*(breadobj[bbreaddish].fats+nonbreadobj[bnondish].fats));
+    var bproteinfullfill=parseInt(bservingsizetemp*(breadobj[bbreaddish].protein+nonbreadobj[bnondish].protein));
+    var lcalfullfilltemp=breadobj[lbreaddish].calories+nonbreadobj[lnondish].calories;
+    var lservingsizetemp=(Lcal/lcalfullfilltemp);
+    var lservingsize=parseInt(lservingsizetemp*(breadobj[lbreaddish].servingSize+nonbreadobj[lnondish].servingSize));
+    var lcalfullfill=parseInt(lservingsizetemp*(breadobj[lbreaddish].calories+nonbreadobj[lnondish].calories));
+    var lcarbfullfill=parseInt(lservingsizetemp*(breadobj[lbreaddish].carbs+nonbreadobj[lnondish].carbs));
+    var lfatfullfill=parseInt(lservingsizetemp*(breadobj[lbreaddish].fats+nonbreadobj[lnondish].fats));
+    var lproteinfullfill=parseInt(lservingsizetemp*(breadobj[lbreaddish].protein+nonbreadobj[lnondish].protein));
+    var dcalfullfilltemp=breadobj[dbreaddish].calories+nonbreadobj[dnondish].calories;
+    var dservingsizetemp=(Dcal/dcalfullfilltemp);
+    var dservingsize=parseInt(dservingsizetemp*(breadobj[dbreaddish].servingSize+nonbreadobj[dnondish].servingSize));
+    var dcalfullfill=parseInt(dservingsizetemp*(breadobj[dbreaddish].calories+nonbreadobj[dnondish].calories));
+    var dcarbfullfill=parseInt(dservingsizetemp*(breadobj[dbreaddish].carbs+nonbreadobj[dnondish].carbs));
+    var dfatfullfill=parseInt(dservingsizetemp*(breadobj[dbreaddish].fats+nonbreadobj[dnondish].fats));
+    var dproteinfullfill=parseInt(dservingsizetemp*(breadobj[dbreaddish].protein+nonbreadobj[dnondish].protein));
+    console.log(bbreaddish.calories);
+
+    res.render("tracker",{login_value:req.isAuthenticated(),
+      bbreadd:breadobj[bbreaddish].name,
+      bnond:nonbreadobj[bnondish].name,
+      lbreadd:breadobj[lbreaddish].name,
+      lnond:nonbreadobj[lnondish].name,
+      dbreadd:breadobj[dbreaddish].name,
+      dnond:nonbreadobj[dnondish].name,
+      bserving:bservingsize,
+      lserving:lservingsize,
+      dserving:dservingsize,
+      bcalf:bcalfullfill,
+      bproteinf:bproteinfullfill,
+      bcarbohydratef:bcarbfullfill,
+      bfatf:bfatfullfill,
+      lcalf:lcalfullfill,
+      lproteinf:lproteinfullfill,
+      lcarbohydratef:lcarbfullfill,
+      lfatf:lfatfullfill,
+      dcalf:dcalfullfill,
+      dproteinf:dproteinfullfill,
+      dcarbohydratef:dcarbfullfill,
+      dfatf:dfatfullfill,
+      bproteinr:Bprotein,
+      bcarbohydrater:Bcarbohydrate,
+      bfatr:Bfat,
+      lproteinr:Lprotein,
+      lcarbohydrater:Lcarbohydrate,
+      lfatr:Lfat,
+      dproteinr:Dprotein,
+      dcarbohydrater:Dcarbohydrate,
+      dfatr:Dfat,
+      bcalr:Bcal,
+      lcalr:Lcal,
+      dcalr:Dcal
+    });
+
   }
   else{
     res.redirect("/");
--- second.js
+++ second.js
@@ -7,24 +7,34 @@ const bodyParser=require("body-parser");
 
         
 
-Object.values(regionalFoodItems).forEach(region=>{
-    console.log();
+// Object.values(regionalFoodItems).forEach(region=>{
+//     console.log();
     
-        Object.values(region).forEach(dish=>{
+//         Object.values(region).forEach(dish=>{
 
-            Object.values(dish).forEach(foodCategory=>{
-                console.log(foodCategory);
+//             Object.values(dish).forEach(foodCategory=>{
+//                 console.log(foodCategory);
         
                 
                 
                 
             
         
-            })
+//             })
                 
-        })
-});
-
+//         })
+// });
+// var awadhi=regionalFoodItems.awadhi;
+// var nonBread = "nonBread";
+// var awad=regionalFoodItems["awadhi"];
+// console.log(awad["nonBread"]);
+var cuisinename="awadhi";
+var foodobj=regionalFoodItems[cuisinename];
+var nonbread=foodobj["nonBread"];
+   var nonbreadlength = Object.keys(foodobj["nonBread"]).length;
+var obj=Object.keys(nonbread)[1];
+console.log(nonbread[obj].name);
+console.log(nonbread["kakhoriKebab"].calories+nonbread[obj].calories); 
 
 /*
 common[0].food_name
